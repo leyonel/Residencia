117a3cc0751b2548b2b02e1edcf08cbbcf30494e
leyonel <leonellayonel@gmail.com>
2022-10-06 17:15:09 -0600

    Create Component Navbar and Page HomePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+// Usar rafce crea un funcion con su export aparte
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
